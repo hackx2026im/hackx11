14e2144c7c9ea6251dd26a40b4da9085cc78a718
Praveen Madawalage <praveenrmadawalage@gmail.com>
2026-07-04 15:28:49 +0530

    fix: Improve preloader sheen effect for better mobile compatibility

--- src/components/SitePreloader.tsx
+++ src/components/SitePreloader.tsx
@@ -35,7 +35,8 @@ export default function SitePreloader() {
       <div className="preloader__logo-wrap">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/hackxlogo.webp" alt="" className="preloader__logo" />
-        <div className="preloader__sheen" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/hackxlogo.webp" alt="" aria-hidden="true" className="preloader__sheen" />
       </div>
     </div>
   );
